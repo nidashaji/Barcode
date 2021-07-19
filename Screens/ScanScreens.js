817add6808b96789949d51854c548d0c70f51477
nidashaji <60589450+nidashaji@users.noreply.github.com>
2021-07-19 13:19:11 -0700

    Add files via upload

---
+++ Screens/ScanScreens.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default class Scanscreen extends React.Component{
+    constructor(){
+        super();
+        this.state ={
+        hasCameraPositions : null,
+        scanned: false,
+        scannedData: '',
+        buttonState: 'normal',
+        }
+    }
+}
+
+function getCameraPositions(){
+
+}
+
+handleBarCodeScanner(){
+
+}
+
